94195ae3224ade807f74c6b8bf8b1c58bbdfb09b
dmitri.lukas <dmitri.lukas@geniussports.com>
2022-12-01 09:11:47 +0200

    Add2

---
+++ Source/EX_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace EX_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>
+                    services
+                        .AddSingleton<ICheck, AgeChecker>()
+                        .AddSingleton<ICheck, GenderChecker>()
+                        .AddSingleton<IPersonProcessor, PersonProcessor>())
+                .Build();
+        
+            TestDI(host.Services);
+        }
+        public static void TestDI(IServiceProvider services)
+        {
+            using IServiceScope serviceScope = services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+        
+            var personProcessor = provider.GetRequiredService<IPersonProcessor>();
+            var Jack = new Person() { Name = "Jack", Age = 32, Gender = Gender.Man };
+            var Jill = new Person() { Name = "Jill", Age = 17, Gender = Gender.Female };
+        
+            personProcessor.RunChecks(Jack);
+            personProcessor.RunChecks(Jill);
+        }
+    }
+}
